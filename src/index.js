d73f199d7e49c3c8370cd76830605c72f752e999
Wendy <honeykatie@qq.com>
2018-02-24 21:20:42 -0800

    Init push for webpack sample project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const MyComp = () => <h1>Hello, world!</h1>;
+
+ReactDOM.render(<MyComp />, document.getElementById('react-container'));
+
+// const arr = ['JavaScript', 'Node', 'webpack', 'React', 'Angular', 'Emmet'];
+//
+// console.log(arr);
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      { // Handle transformation,
+        test: /.js$/, // 作用于出了node_module之外所有的.js文件
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
+        }
+      }
+    ]
+  }
+};
